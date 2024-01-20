5534f078e8363c0f4df0d0108ac397016dff9231
Schindler <eternalworm6@gmail.com>
2024-01-20 13:44:05 -0300

    atualizando condicional

--- logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -1,6 +1,9 @@
 alert(`Boas vindas ao jogo do número secreto!`);
 let numeroSecreto = 29;
+console.log(numeroSecreto);
 let chute = prompt(`chute um número entre 1 e 30`);
-if (chute != numeroSecreto) {
+if (chute == numeroSecreto) {
+    alert(`você acertou o número secreto que é ${chute}`);
+}else{
     alert(`você errou o número secreto não é ${chute}`);
 }
