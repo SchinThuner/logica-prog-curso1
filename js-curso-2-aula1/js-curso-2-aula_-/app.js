defce7243ca342f96139438b00b14ee74b27f38e
Schindler <eternalworm6@gmail.com>
2024-01-25 21:31:05 -0300

    Implementation of the function to display text on the screen and refactoring the code

--- js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -1,9 +1,13 @@
-let titulo = document.querySelector('h1');
-let paragrafo = document.querySelector('p');
-titulo.innerHTML = `Jogo do número Secreto`;
-paragrafo.innerHTML = `Escolha um número entre 1 e 10`;
-let chute = '';
-function verificarChute(){
-    chute = document.getElementsByClassName('container__input').value;
-    console.log(`valor do chute ${chute}`);
+function showOnScreen(tag,text) {
+    let fieldText = document.querySelector(tag);
+    fieldText.innerHTML = `${text}`;
+}
+let limitNumber = 10;
+showOnScreen(`h1`,`Jogo do número Secreto`);
+showOnScreen(`p`,`Escolha um número entre 1 e ${limitNumber}`);
+
+let Attempt = '';
+function checkAttempt(){
+    Attempt = document.getElementsByClassName('container__input').value;
+    console.log(`valor do chute ${Attempt}`);
 }
